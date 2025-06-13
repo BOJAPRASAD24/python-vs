2a1e1c0f9001289cb7afe6ff12493d26943123c3
BOJAPRASAD24 <bojaprasad1998@gmail.com>
2025-06-13 13:56:54 +0530

    Initial commit

---
+++ main.py
@@ -0,0 +1,27 @@
+name = "Boja"
+print("Hi "+name)
+
+import keyword
+print(keyword.kwlist)
+
+name = 'Boja'
+print("He like's dog.")
+para = """This is a
+
+sample text."""
+print(para)
+
+"""
+This is a comment
+"""
+
+name= "Boja"
+age = "25"
+
+# print("Hi my name is " + name + " and I am " + age + " years old.")
+print(f"Hi my name is {name} and I am {age}years old.")
+
+
+
+
+       
