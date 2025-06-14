960c214a6fba6de13e094a0355e0594e1adf112d
BOJAPRASAD24 <bojaprasad1998@gmail.com>
2025-06-14 13:11:41 +0530

    Added math and random function examples

--- main.py
+++ main.py
@@ -22,6 +22,39 @@ age = "25"
 #print(f"Hi my name is {name} and I am {age}year old.")
 print("Hi my name is {} and I am {} years old.".format(name, age))
 
+a + bj
+
+a = 0
+a = -13.3
+a = 2 + 3j
+a = -1j
+print(a)
+
+a = 10
+b = 3
+
+# print(a+b)
+# print(a-b)
+# print(a*b)
+# print(a/b)
+# print(a%b)
+# print(a**b)
+
+# import math
+
+# print(math.sqrt(16))
+# print(math.pow(10,3))
+# print(math.floor(3.99))
+# print(math.celi(4.2))
+
+# import random
+
+# print(random.random())
+# print(random.randit(1,10))
+# print(random.choice([2, 3, 5, 6, 8]))
+
+FILE_SIZE=1_00_000
+print(FILE_SIZE)
 
 
 
