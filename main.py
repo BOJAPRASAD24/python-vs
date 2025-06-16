18ce5a4cd90e09ca066dc19f6ea0d96884e9c889
BOJAPRASAD24 <bojaprasad1998@gmail.com>
2025-06-16 12:23:30 +0530

    add main.py

--- main.py
+++ main.py
@@ -22,8 +22,6 @@ age = "25"
 #print(f"Hi my name is {name} and I am {age}year old.")
 print("Hi my name is {} and I am {} years old.".format(name, age))
 
-a + bj
-
 a = 0
 a = -13.3
 a = 2 + 3j
@@ -38,23 +36,89 @@ b = 3
 # print(a*b)
 # print(a/b)
 # print(a%b)
+# print(a//b)
 # print(a**b)
-
-# import math
-
-# print(math.sqrt(16))
-# print(math.pow(10,3))
-# print(math.floor(3.99))
-# print(math.celi(4.2))
 
 # import random
 
 # print(random.random())
-# print(random.randit(1,10))
+# print(random.randint(1,10))
 # print(random.choice([2, 3, 5, 6, 8]))
 
 FILE_SIZE=1_00_000
 print(FILE_SIZE)
+
+# a = 10
+# b = 3
+
+# print(a <= b)
+
+# if (cond):
+    # if-block
+
+# age = int(input("Enter your age: "))
+                
+# if age>=18:
+
+#     print("you can vote!")
+# else:
+#     print("you cannot vote.")
+
+# if age<5:
+#     print("you have to pay 50.")
+# elif age < 16:
+#     print("you have to pay 100.")
+# else:
+#     print("you have to pay 200.")
+
+# age = int(input("Enter your age: "))  
+
+# value_if_true if condition else value_if_false
+
+# result = "you can vote!" if age >=18 else "you cannot vote."
+# print(result)
+
+# for _ in range(6):
+#     print("Hello World")
+
+# for i in range(2,7):
+#     print(i)
+
+# for i in range(2,12,2):
+#     print(i)
+
+max = 5
+counter = 0
+
+while counter < max:
+    # 0 < 5
+    # 1 < 5
+    # 2 < 5
+    # 3 < 5
+    # 4 < 5
+    # 5 < 5
+
+    print(counter)
+    counter +=1
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
